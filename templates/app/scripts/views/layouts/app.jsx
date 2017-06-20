de6e4f2dff068353e8bca0a793533d40294dc0dd
elf-mouse <ifmouz@gmail.com>
2017-06-20 11:31:10 +0800

    update

--- templates/app/scripts/views/layouts/app.jsx
+++ templates/app/scripts/views/layouts/app.jsx
@@ -4,19 +4,14 @@ import routes from '../../routers/index';
 import RouteWithSubRoutes from '../../routers/config';
 import MyMenu from '../components/menu';
 import Home from '../home';
+import logo from '../../../images/logo.svg';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.logo = require('../../../images/logo.svg');
-  }
-
   render() {
     return (
       <div className="app">
         <div className="app-header">
-          <img src={this.logo} className="app-logo" alt="logo" />
+          <img src={logo} className="app-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="app-intro">thx <a href="//balmjs.com/">BalmJS</a></p>
