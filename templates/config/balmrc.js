e9c510fc5e033df16c8a312b3bb6cc4088cb53fa
elf-mouse <ifmouz@gmail.com>
2017-07-06 14:10:04 +0800

    update config

---
+++ templates/config/balmrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  server: {
+    open: true,
+    proxyTable: {
+      '/api': {
+        target: 'http://your.project.dev',
+        changeOrigin: true
+      }
+    }
+  },
+  roots: {
+    source: 'app'
+  },
+  paths: {
+    source: {
+      css: 'styles',
+      js: 'scripts',
+      img: 'images'
+    }
+  },
+  styles: {
+    ext: 'css', // PostCSS
+    autoprefixer: [
+      '> 1%',
+      'last 2 versions',
+      'not ie <= 8'
+    ]
+  },
+  scripts: {
+    entry: {
+      react: ['react', 'react-dom', 'react-router-dom'],
+      main: './app/scripts/main.jsx' // Entry js file
+    }
+  },
+  cache: true,
+  assets: {
+    root: 'assets', // Replace 'assets' to your remote project root
+    publicPath: 'public'
+  }
+  // More Config
+  // https://github.com/balmjs/balm/blob/master/docs/configuration.md
+};
--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,37 +1,8 @@
 var balm = require('balm');
-var config = require('./balmrc');
+var config = require('./config/balmrc');
+var templates = require('./config/publish');
 
-balm.config = {
-  server: {
-    open: true,
-    proxyTable: config.proxyTable
-  },
-  roots: {
-    source: 'app'
-  },
-  paths: {
-    source: {
-      css: 'styles',
-      js: 'scripts',
-      img: 'images'
-    }
-  },
-  styles: {
-    ext: 'css', // PostCSS
-    autoprefixer: [
-      '> 1%',
-      'last 2 versions',
-      'not ie <= 8'
-    ]
-  },
-  scripts: {
-    entry: config.entry
-  },
-  cache: true,
-  assets: config.assets
-  // More Config
-  // https://github.com/balmjs/balm/blob/master/docs/configuration.md
-};
+balm.config = config;
 
 balm.go(function(mix) {
   if (balm.config.production) {
@@ -41,8 +12,8 @@ balm.go(function(mix) {
     // Publish assets
     mix.publish();
     // Publish templates
-    Object.keys(config.publish).forEach(function(key) {
-      mix.publish(key, config.publish[key].target, config.publish[key].option || {});
+    Object.keys(templates).forEach(function(key) {
+      mix.publish(key, templates[key].target, templates[key].option || {});
     });
   }
 });
