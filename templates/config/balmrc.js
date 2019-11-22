85c762962fd0a2c40d7e814cbbd76095ba8922f1
Elf-mousE <ifmouz@gmail.com>
2019-11-22 17:33:25 +0800

    update balm@2.0.0

--- templates/config/balmrc.js
+++ templates/config/balmrc.js
@@ -1,28 +1,24 @@
 /*eslint-env node*/
 const path = require('path');
 
-// Documentation - http://balmjs.com/docs/en/configuration/toc.html
-// 中文文档 - http://balmjs.com/docs/zh-cn/configuration/toc.html
+// Documentation - http://balmjs.com/docs/v2/config/
+// 中文文档 - https://balmjs.com/docs/v2/zh/config/
 module.exports = {
   server: {
     open: true,
-    proxyContext: '/api',
-    proxyOptions: {
-      target: 'http://your.project.dev', // Target host
-      changeOrigin: true // Needed for virtual hosted sites
+    proxyConfig: {
+      context: '/api',
+      options: {
+        target: 'http://your.project.dev', // Target host
+        changeOrigin: true // Needed for virtual hosted sites
+      }
     }
   },
   roots: {
     source: 'app'
   },
-  paths: {
-    source: {
-      css: 'styles',
-      js: 'scripts'
-    }
-  },
   styles: {
-    ext: 'scss'
+    extname: 'scss'
   },
   scripts: {
     entry: {
@@ -33,10 +29,10 @@ module.exports = {
       '@': path.resolve(__dirname, '..', 'app', 'scripts')
     }
   },
-  cache: true,
   assets: {
     root: 'assets', // Replace 'assets' to your remote project root
-    mainDir: 'public'
+    mainDir: 'public',
+    cache: true
   }
   // More Config
 };
--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -5,15 +5,13 @@ const templates = require('./config/publish');
 balm.config = config;
 
 balm.go(mix => {
-  if (balm.config.isProd) {
+  if (mix.env.isProd) {
     // For test data
     mix.copy('./app/data/*', './dist/data');
 
     // Publish assets
     mix.publish();
     // Publish templates
-    Object.keys(templates).forEach(key => {
-      mix.publish(key, templates[key].target, templates[key].option || {});
-    });
+    mix.publish(templates);
   }
 });
