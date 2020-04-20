f421db5f6db620b9257585805df03ddc4d1264a9
Elf-mousE <ifmouz@gmail.com>
2020-04-20 10:26:20 +0800

    update

--- templates/config/balmrc.js
+++ templates/config/balmrc.js
@@ -5,14 +5,13 @@ const path = require('path');
 // 中文文档 - https://balmjs.com/docs/v2/zh/config/
 module.exports = {
   server: {
-    open: true,
-    proxyConfig: {
-      context: '/api',
-      options: {
-        target: 'http://your.project.dev', // Target host
-        changeOrigin: true // Needed for virtual hosted sites
-      }
-    }
+    // proxyConfig: {
+    //   context: '/api',
+    //   options: {
+    //     target: 'http://your.project.dev', // Target host
+    //     changeOrigin: true // Needed for virtual hosted sites
+    //   }
+    // }
   },
   roots: {
     source: 'app'
